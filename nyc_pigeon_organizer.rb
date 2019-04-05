5d21c57a0ffa688201ff08e9398ffc705aeba457
dpatel0114 <1991dharapatel@gmail.com>
2019-04-04 22:27:20 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash ={}
+  # for element in data.keys()
+  #   for x in data[element].keys()
+  #   end
+  # end
+  # new_hash
+  #hey
 end
