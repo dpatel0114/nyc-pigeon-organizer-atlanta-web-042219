db9837ae6d96c5bbacb56fbcd8b475a1df54beb5
dpatel0114 <1991dharapatel@gmail.com>
2019-04-14 18:24:23 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,37 @@
-def nyc_pigeon_organizer(data)
+require 'pry'
+
+def nyc_pigeon_organizer(pigeon_data)
   # write your code here!
+  new_array = []
+
+
+  for first in pigeon_data.keys
+    for second in pigeon_data[first].keys
+
+      # for third in pigeon_data[first][second]
+      new_array << pigeon_data[first][second]
+    end
+  end
+  new_array = new_array.flatten.uniq
   new_hash ={}
-  # for element in data.keys()
-  #   for x in data[element].keys()
-  #   end
-  # end
-  # new_hash
-  #hey
-end
+
+  new_array.each do |bird|
+    new_hash[bird] = {}
+    pigeon_data.each do |key, value|
+
+      value.each do |attribute, element|
+        # binding.pry
+
+          if element.include?(bird)
+            # binding.pry
+            if new_hash[bird].keys.include?(key)
+                new_hash[bird][key] << attribute.to_s
+            else
+              new_hash[bird][key] = [attribute.to_s]
+          end
+        end
+      end
+    end
+  end
+new_hash
+  end
